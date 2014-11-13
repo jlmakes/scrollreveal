d03f7e0ba3723ea0ea5d6219f23d6fd208e4c9f1
Julian Lloyd <julianlloyd@gmail.com>
2014-11-13 15:11:32 -0800

    feat(*): Update Gulp tasks.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,29 @@
-var gulp   = require('gulp'),
-    uglify = require('gulp-uglify'),
-    umd    = require('gulp-wrap-umd'),
-    rename = require('gulp-rename');
+var gulp       = require( 'gulp' )
+  , uglify     = require( 'gulp-uglify' )
+  , umd        = require( 'gulp-wrap-umd' )
+  , rename     = require( 'gulp-rename' )
+  , livereload = require( 'gulp-livereload' )
 
-gulp.task('wrap', function(){
-  gulp.src('scrollReveal.js')
-    .pipe(umd({ namespace: 'scrollReveal', exports: 'scrollReveal' }))
-    .pipe(gulp.dest('dist'))
-});
+gulp.task( 'wrap', function() {
 
-gulp.task('minify', function(){
-  gulp.src('scrollReveal.js')
-    .pipe(umd({ namespace: 'scrollReveal', exports: 'scrollReveal' }))
-    .pipe(uglify())
-    .pipe(rename('scrollReveal.min.js'))
-    .pipe(gulp.dest('dist'))
-});
+  gulp.src( 'scrollReveal.js' )
+    .pipe( umd({ namespace: 'scrollReveal', exports: 'scrollReveal' }) )
+    .pipe( gulp.dest( 'dist' ) )
+})
 
-gulp.task('default', ['wrap', 'minify']);
+gulp.task( 'minify', function() {
+
+  gulp.src( 'scrollReveal.js' )
+    .pipe( umd({ namespace: 'scrollReveal', exports: 'scrollReveal' }) )
+    .pipe( uglify() )
+    .pipe( rename( 'scrollReveal.min.js' ) )
+    .pipe( gulp.dest( 'dist' ) )
+})
+
+gulp.task( 'default', function() {
+
+  livereload.listen()
+  gulp.watch([ 'dev/*.html', '*.js']).on( 'change', livereload.changed )
+})
+
+gulp.task( 'build', [ 'wrap', 'minify' ])
