5bac063d4384df0845e0571eb817d297986fe54a
J.L. Makes <julianlloyd@gmail.com>
2015-12-09 07:26:10 +0000

    cleanup(*)

--- ScrollReveal.js
+++ ScrollReveal.js
@@ -5,33 +5,42 @@
          ___/ / /__/ /  / /_/ / / / _, _/  __/ |/ /  __/ /_/ / /
         /____/\___/_/   \____/_/_/_/ |_|\___/|___/\___/\__,_/_/   v3.0.0
 
-________________________________________________________________________________
+////////////////////////////////////////////////////////////////////////////////
 
-      Copyright 2015 Julian Lloyd (@jlmakes) Open source under MIT license
-______________________________________________________________________________*/
+      Copyright 2016 Julian Lloyd (@jlmakes) Open source under MIT license
+
+//////////////////////////////////////////////////////////////////////////////*/
 
 window.ScrollReveal = (function( window ){
 
   'use strict';
 
-  var _animate
+  var self
+
+    // private methods
+    , _animate
     , _cleanDOM
     , _configFactory
+    , _handler
+    , _styleFactory
+    , _updateElemStore
+
+    // local utils
+    , _getViewportScrolled
+    , _getViewportSize
+    , _isElemVisible
+    , _isSupported
+
+    // generic utils
     , _extend
     , _extendClone
-    , _getOffset
-    , _getScrolled
-    , _getViewportSize
-    , _handler
-    , _isElemVisible
+    , _getElOffset
     , _isMobile
     , _isNode
     , _isObject
-    , _isSupported
-    , _requestAnimFrame
-    , _styleFactory
-    , _updateElemStore
-    , self;
+
+    // polyfill
+    , _requestAnimFrame;
 
   function ScrollReveal( config ){
 
@@ -56,7 +65,7 @@ window.ScrollReveal = (function( window ){
     self.initialized = false; // Flag used for 'onload' delay styles
   }
 
-
+  //////////////////////////////////////////////////////////////////////////////
 
   ScrollReveal.prototype.defaults = {
 
@@ -95,7 +104,7 @@ window.ScrollReveal = (function( window ){
     afterReset:  function( domEl ){}
   };
 
-
+  //////////////////////////////////////////////////////////////////////////////
 
   ScrollReveal.prototype.init = function(){
 
@@ -124,7 +133,7 @@ window.ScrollReveal = (function( window ){
     return self;
   };
 
-
+  //////////////////////////////////////////////////////////////////////////////
 
   ScrollReveal.prototype.reveal = function( selector, config ){
 
@@ -189,7 +198,7 @@ window.ScrollReveal = (function( window ){
     return self;
   };
 
-
+  // Private Methods ///////////////////////////////////////////////////////////
 
   _animate = function(){
 
@@ -277,7 +286,17 @@ window.ScrollReveal = (function( window ){
     }
   };
 
+  //////////////////////////////////////////////////////////////////////////////
 
+  _cleanDOM = function(){
+    for ( var key in self.store.elements ){
+      if ( self.store.elements.hasOwnProperty( key ) ){
+        self.store.elements[ key ].domEl.removeAttribute('data-sr-id');
+      }
+    }
+  };
+
+  //////////////////////////////////////////////////////////////////////////////
 
   _configFactory = function( config, context ){
 
@@ -310,8 +329,8 @@ window.ScrollReveal = (function( window ){
       config.axis = 'X';
     }
 
-    // Let’s make sure our our pixel distances are negative for top and left.
-    // e.g. "enter top and move 25px" starts at 'top: -25px' in CSS
+    // Let’s make sure our our pixel distances are negative for top and left
+    // e.g. { enter: 'top', move: '25px' } starts at 'top: -25px' in CSS
 
     if ( config.enter === 'top' || config.enter === 'left' ){
       config.move = '-' + config.move;
@@ -320,7 +339,16 @@ window.ScrollReveal = (function( window ){
     return config;
   };
 
+  //////////////////////////////////////////////////////////////////////////////
 
+  _handler = function(){
+    if ( !self.blocked ){
+      self.blocked = true;
+      _requestAnimFrame( _animate );
+    }
+  };
+
+  //////////////////////////////////////////////////////////////////////////////
 
   _styleFactory = function( elem ){
 
@@ -340,7 +368,7 @@ window.ScrollReveal = (function( window ){
       // Stuff...
     }
 
-    // Capture the original inline styles.
+    // Capture the original inline styles
 
     if ( !elem.styles ){
       original = elem.domEl.getAttribute('style')
@@ -350,10 +378,10 @@ window.ScrollReveal = (function( window ){
 
     // Since JavaScript runs after the page has begun rendering, it’s possible
     // that elements will be seen before ScrollReveal can hide them during
-    // initialization
-
-    // One technique is to apply `visibility: hidden` via CSS to your elements
-    // so they load hidden, allowing ScrollReveal to overwrite visibility here
+    // initialization...
+    //
+    // The solution is using `visibility: hidden` (CSS) on your elements to
+    // ensure they load hidden, and here is where we handle that expectation
 
     if ( original ){
       inline = original + '; visibility: visible; ';
@@ -411,6 +439,8 @@ window.ScrollReveal = (function( window ){
       transition: transition
     };
 
+    // _styleFactory Methods -------------------------------------------------//
+
     function generateStyles( t ){
 
       if ( parseInt( config.move ) !== 0 ){
@@ -452,7 +482,37 @@ window.ScrollReveal = (function( window ){
     }
   };
 
+  //////////////////////////////////////////////////////////////////////////////
 
+  _updateElemStore = function( elem ){
+
+    self.store.elements[ elem.id ] = elem;
+
+    if ( self.store.viewports.indexOf( elem.config.viewport ) == -1 ){
+      self.store.viewports.push( elem.config.viewport );
+    }
+  };
+
+  // Local Utils ///////////////////////////////////////////////////////////////
+
+  _getViewportScrolled = function( viewport ){
+
+    if ( viewport === window.document.documentElement ){
+      return {
+        x: window.pageXOffset,
+        y: window.pageYOffset
+      };
+    }
+
+    else {
+      return {
+        x: viewport.scrollLeft + viewport.offsetLeft,
+        y: viewport.scrollTop + viewport.offsetTop
+      };
+    }
+  };
+
+  //////////////////////////////////////////////////////////////////////////////
 
   _getViewportSize = function( viewport ){
 
@@ -476,57 +536,13 @@ window.ScrollReveal = (function( window ){
     };
   };
 
-
-
-  _getScrolled = function( viewport ){
-
-    if ( viewport === window.document.documentElement ){
-      return {
-        x: window.pageXOffset,
-        y: window.pageYOffset
-      };
-    }
-
-    else {
-      return {
-        x: viewport.scrollLeft + viewport.offsetLeft,
-        y: viewport.scrollTop + viewport.offsetTop
-      };
-    }
-  };
-
-
-
-  _getOffset = function( domEl ){
-
-    var offsetTop  = 0;
-    var offsetLeft = 0;
-
-    do {
-
-      if ( !isNaN( domEl.offsetTop ) ){
-        offsetTop  += domEl.offsetTop;
-      }
-
-      if ( !isNaN( domEl.offsetLeft ) ){
-        offsetLeft += domEl.offsetLeft;
-      }
-
-    } while ( domEl = domEl.offsetParent );
-
-    return {
-      top:  offsetTop,
-      left: offsetLeft
-    };
-  };
-
-
+  //////////////////////////////////////////////////////////////////////////////
 
   _isElemVisible = function( elem ){
 
-    var offset   = _getOffset( elem.domEl )
+    var offset   = _getElOffset( elem.domEl )
       , viewport = _getViewportSize( elem.config.viewport )
-      , scrolled = _getScrolled( elem.config.viewport )
+      , scrolled = _getViewportScrolled( elem.config.viewport )
 
       , elHeight = elem.domEl.offsetHeight
       , elWidth  = elem.domEl.offsetWidth
@@ -539,6 +555,8 @@ window.ScrollReveal = (function( window ){
       , vFactor  = elem.config.vFactor;
 
     return ( confirmBounds() || isPositionFixed() );
+
+    // _isElemVisible Methods ------------------------------------------------//
 
     function confirmBounds(){
 
@@ -563,16 +581,7 @@ window.ScrollReveal = (function( window ){
     }
   };
 
-
-
-  _isMobile = function(){
-
-    var agent = navigator.userAgent || navigator.vendor || window.opera;
-
-    return (/(ipad|playbook|silk|android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test( agent )||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test( agent.substr( 0, 4 ))) ? true : false;
-  };
-
-
+  //////////////////////////////////////////////////////////////////////////////
 
   _isSupported = function(){
 
@@ -589,33 +598,7 @@ window.ScrollReveal = (function( window ){
     return true;
   };
 
-
-
-  _cleanDOM = function(){
-    for ( var key in self.store.elements ){
-      if ( self.store.elements.hasOwnProperty( key ) ){
-        self.store.elements[ key ].domEl.removeAttribute('data-sr-id');
-      }
-    }
-  };
-
-// Private Methods /////////////////////////////////////////////////////////////
-
-  _updateElemStore = function( elem ){
-
-    self.store.elements[ elem.id ] = elem;
-
-    if ( self.store.viewports.indexOf( elem.config.viewport ) == -1 ){
-      self.store.viewports.push( elem.config.viewport );
-    }
-  };
-
-  _handler = function( e ){
-    if ( !self.blocked ){
-      self.blocked = true;
-      _requestAnimFrame( _animate );
-    }
-  };
+  // Generic Utils /////////////////////////////////////////////////////////////
 
   _extend = function( target, src ){
     for ( var prop in src ){
@@ -637,10 +620,48 @@ window.ScrollReveal = (function( window ){
     return target;
   };
 
+  //////////////////////////////////////////////////////////////////////////////
+
   _extendClone = function( target, src ){
     var clone = {};
     return _extend( _extend( clone, target ), src );
   };
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  _getElOffset = function( domEl ){
+
+    var offsetTop  = 0;
+    var offsetLeft = 0;
+
+    do {
+
+      if ( !isNaN( domEl.offsetTop ) ){
+        offsetTop  += domEl.offsetTop;
+      }
+
+      if ( !isNaN( domEl.offsetLeft ) ){
+        offsetLeft += domEl.offsetLeft;
+      }
+
+    } while ( domEl = domEl.offsetParent );
+
+    return {
+      top:  offsetTop,
+      left: offsetLeft
+    };
+  };
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  _isMobile = function(){
+
+    var agent = navigator.userAgent || navigator.vendor || window.opera;
+
+    return (/(ipad|playbook|silk|android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test( agent )||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test( agent.substr( 0, 4 ) ) ) ? true : false;
+  };
+
+  //////////////////////////////////////////////////////////////////////////////
 
   _isNode = function( obj ){
     return (
@@ -648,6 +669,8 @@ window.ScrollReveal = (function( window ){
       obj && typeof obj === "object" && obj !== null && obj.nodeType === 1 && typeof obj.nodeName==="string"
     );
   };
+
+  //////////////////////////////////////////////////////////////////////////////
 
   _isObject = function( obj ){
     return ( typeof obj === 'object' && obj.constructor == Object );
