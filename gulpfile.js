6df6ede8e053b93afe59eba179324c6d606cf796
J.L. Makes <julianlloyd@gmail.com>
2015-12-15 14:10:25 +0000

    gulp(development): replace livereload with browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,27 @@
-var gulp       = require( 'gulp' )
-  , uglify     = require( 'gulp-uglify' )
-  , umd        = require( 'gulp-wrap-umd' )
-  , rename     = require( 'gulp-rename' )
-  , livereload = require( 'gulp-livereload' )
+var gulp    = require('gulp');
+var uglify  = require('gulp-uglify');
+var umd     = require('gulp-wrap-umd');
+var rename  = require('gulp-rename');
+var browser = require('browser-sync').create();
 
-gulp.task( 'wrap', function() {
 
-  gulp.src( 'ScrollReveal.js' )
+gulp.task( 'dist', function() {
+  gulp.src('scrollreveal.js')
     .pipe( umd({ namespace: 'ScrollReveal', exports: 'ScrollReveal' }) )
-    .pipe( gulp.dest( 'dist' ) )
-})
+    .pipe( gulp.dest('dist') )
+});
 
-gulp.task( 'minify', function() {
-
-  gulp.src( 'ScrollReveal.js' )
+gulp.task( 'dist:minify', function() {
+  gulp.src('scrollreveal.js')
     .pipe( umd({ namespace: 'ScrollReveal', exports: 'ScrollReveal' }) )
     .pipe( uglify() )
-    .pipe( rename( 'ScrollReveal.min.js' ) )
-    .pipe( gulp.dest( 'dist' ) )
+    .pipe( rename('scrollreveal.min.js') )
+    .pipe( gulp.dest('dist') )
 })
 
 gulp.task( 'default', function() {
+  browser.init({ server: { baseDir: "./dev" } });
+  gulp.watch([ 'dev/*.html', '*.js' ]).on( "change", browser.reload );
+});
 
-  livereload.listen()
-  gulp.watch([ 'dev/*.html', '*.js' ]).on( 'change', livereload.changed )
-})
-
-gulp.task( 'build', [ 'wrap', 'minify' ])
+gulp.task( 'build', [ 'dist', 'dist:minify' ]);
