4683adb30310ada1ed8f22e59a075895a0ca0189
J.L. Makes <julianlloyd@gmail.com>
2016-01-13 07:03:37 +0100

    formatting(*)

--- scrollreveal.js
+++ scrollreveal.js
@@ -11,17 +11,17 @@
     https://scrollrevealjs.org — https://github.com/jlmakes/scrollreveal.js
 ______________________________________________________________________________*/
 
-(function() {
+(function(){
   var Tools, sr, _requestAnimationFrame;
 
-  this.ScrollReveal = (function() {
+  this.ScrollReveal = (function(){
     ScrollReveal.prototype.defaults = {
       // Animation
       origin      : 'bottom',
       distance    : '20px',
       duration    : 500,
       delay       : 0,
-      rotate      : { x: 0, y: 0, z: 0 },
+      rotate      : { x : 0, y : 0, z : 0 },
       opacity     : 0,
       scale       : 0.9,
       easing      : 'cubic-bezier( 0.6, 0.2, 0.1, 1 )',
@@ -31,22 +31,22 @@ ______________________________________________________________________________*/
       reset       : false,
       useDelay    : 'always',
       viewFactor  : 0.2,
-      viewOffset  : { top: 0, right: 0, bottom: 0, left: 0 },
-      afterReveal : function( domEl ) {},
-      afterReset  : function( domEl ) {}
+      viewOffset  : { top : 0, right : 0, bottom : 0, left : 0 },
+      afterReveal : function( domEl ){},
+      afterReset  : function( domEl ){}
     };
 
-    function ScrollReveal( config ) {
-      if ( window == this ) {
+    function ScrollReveal( config ){
+      if ( window == this ){
         return new ScrollReveal( config );
       }
       sr = this;
       sr.tools = new Tools();
       sr.tools.extend( sr.defaults, config || {} );
 
-      if ( sr.tools.isMobile() && !sr.defaults.mobile ) {
+      if ( sr.tools.isMobile() && !sr.defaults.mobile ){
         return false;
-      } else if ( !sr.tools.isSupported('transform') || !sr.tools.isSupported('transition') ) {
+      } else if ( !sr.tools.isSupported('transform') || !sr.tools.isSupported('transition') ){
         return console.warn('ScrollReveal is not supported in this browser.');
       }
 
@@ -61,27 +61,27 @@ ______________________________________________________________________________*/
       return sr;
     }
 
-    ScrollReveal.prototype.reveal = function( selector, config, sync ) {
+    ScrollReveal.prototype.reveal = function( selector, config, sync ){
       var elements, container, elem, elemId;
 
-      if ( config && config.container ) {
+      if ( config && config.container ){
         container = config.container;
-      } else if ( sr.defaults.container ) {
+      } else if ( sr.defaults.container ){
         container = sr.defaults.container;
       } else {
         container = window.document.documentElement;
       }
 
       elements = Array.prototype.slice.call( container.querySelectorAll( selector ) );
-      if ( !elements.length ) {
+      if ( !elements.length ){
         console.warn('reveal(\'' + selector + '\') failed: no elements found.');
         return sr;
       }
-      for ( var i = 0; i < elements.length; i++ ) {
+      for ( var i = 0; i < elements.length; i++ ){
         elem   = {}
         elemId = elements[ i ].getAttribute('data-sr-id');
 
-        if ( elemId ) {
+        if ( elemId ){
           elem = sr.store.elements[ elemId ];
         } else {
           elem = {
@@ -97,16 +97,16 @@ ______________________________________________________________________________*/
         sr.style( elem );
         sr.updateStore( elem );
 
-        if ( !elem.revealed ) {
+        if ( !elem.revealed ){
           elem.domEl.setAttribute( 'style',
               elem.styles.inline
             + elem.styles.transform.initial
           );
         }
       }
-      if ( !sync ) {
+      if ( !sync ){
         sr.record( selector, config );
-        if ( sr.initTimeout ) {
+        if ( sr.initTimeout ){
           window.clearTimeout( sr.initTimeout );
         }
         sr.initTimeout = window.setTimeout( sr.init, 0 );
@@ -114,29 +114,29 @@ ______________________________________________________________________________*/
       return sr;
     };
 
-    ScrollReveal.prototype.configure = function( elem, config ) {
-      if ( !elem.config ) {
+    ScrollReveal.prototype.configure = function( elem, config ){
+      if ( !elem.config ){
         elem.config = sr.tools.extendClone( sr.defaults, config );
       } else {
         elem.config = sr.tools.extendClone( elem.config, config );
       }
 
-      if ( elem.config.origin === 'top' || elem.config.origin === 'bottom' ) {
+      if ( elem.config.origin === 'top' || elem.config.origin === 'bottom' ){
         elem.config.axis = 'Y';
       } else {
         elem.config.axis = 'X';
       }
 
-      if ( elem.config.origin === 'top' || elem.config.origin === 'left' ) {
+      if ( elem.config.origin === 'top' || elem.config.origin === 'left' ){
         elem.config.distance = '-' + elem.config.distance;
       }
     };
 
-    ScrollReveal.prototype.style = function( elem ) {
+    ScrollReveal.prototype.style = function( elem ){
       var config   = elem.config;
       var computed = window.getComputedStyle( elem.domEl );
 
-      if ( !elem.styles ) {
+      if ( !elem.styles ){
         elem.styles = {
           transition : {},
           transform  : {},
@@ -146,7 +146,7 @@ ______________________________________________________________________________*/
         elem.styles.inline          += '; visibility: visible; ';
         elem.styles.computed.opacity = computed.opacity;
 
-        if ( !computed.transition || computed.transition == 'all 0s ease 0s' ) {
+        if ( !computed.transition || computed.transition == 'all 0s ease 0s' ){
           elem.styles.computed.transition = '';
         } else {
           elem.styles.computed.transition = computed.transition + ', ';
@@ -167,24 +167,24 @@ ______________________________________________________________________________*/
       elem.styles.transform.target  += 'transform:';
       generateTransform( elem.styles.transform );
 
-      function generateTransform( transform ) {
-        if ( parseInt( config.distance ) ) {
+      function generateTransform( transform ){
+        if ( parseInt( config.distance ) ){
           transform.initial += ' translate' + config.axis + '(' + config.distance + ')';
           transform.target  += ' translate' + config.axis + '(0)';
         }
-        if ( config.scale ) {
+        if ( config.scale ){
           transform.initial += ' scale(' + config.scale + ')';
           transform.target  += ' scale(1)';
         }
-        if ( config.rotate.x ) {
+        if ( config.rotate.x ){
           transform.initial += ' rotateX(' + config.rotate.x + 'deg)';
           transform.target  += ' rotateX(0)';
         }
-        if ( config.rotate.y ) {
+        if ( config.rotate.y ){
           transform.initial += ' rotateY(' + config.rotate.y + 'deg)';
           transform.target  += ' rotateY(0)';
         }
-        if ( config.rotate.z ) {
+        if ( config.rotate.z ){
           transform.initial += ' rotateZ(' + config.rotate.z + 'deg)';
           transform.target  += ' rotateZ(0)';
         }
@@ -193,15 +193,15 @@ ______________________________________________________________________________*/
       }
     };
 
-    ScrollReveal.prototype.updateStore = function( elem ) {
+    ScrollReveal.prototype.updateStore = function( elem ){
       var container = elem.config.container;
-      if ( container && sr.store.containers.indexOf( container ) == -1 ) {
+      if ( container && sr.store.containers.indexOf( container ) == -1 ){
         sr.store.containers.push( elem.config.container );
       }
       sr.store.elements[ elem.id ] = elem;
     };
 
-    ScrollReveal.prototype.record = function( selector, config ) {
+    ScrollReveal.prototype.record = function( selector, config ){
       var record = {
         selector : selector,
         config   : config
@@ -209,13 +209,13 @@ ______________________________________________________________________________*/
       sr.history.push( record );
     };
 
-    ScrollReveal.prototype.init = function() {
+    ScrollReveal.prototype.init = function(){
       sr.animate();
-      for ( var i = 0; i < sr.store.containers.length; i++ ) {
+      for ( var i = 0; i < sr.store.containers.length; i++ ){
         sr.store.containers[ i ].addEventListener( 'scroll', sr.handler );
         sr.store.containers[ i ].addEventListener( 'resize', sr.handler );
       }
-      if ( !sr.initialized ) {
+      if ( !sr.initialized ){
         window.addEventListener( 'scroll', sr.handler );
         window.addEventListener( 'resize', sr.handler );
         sr.initialized = true;
@@ -223,8 +223,8 @@ ______________________________________________________________________________*/
       return sr;
     };
 
-    ScrollReveal.prototype.handler = function() {
-      if ( !sr.running ) {
+    ScrollReveal.prototype.handler = function(){
+      if ( !sr.running ){
         _requestAnimationFrame(function(){
           sr.running = true;
           sr.animate();
@@ -232,17 +232,17 @@ ______________________________________________________________________________*/
       }
     };
 
-    ScrollReveal.prototype.animate = function() {
+    ScrollReveal.prototype.animate = function(){
       var elem, visible;
 
-      sr.tools.forOwn( sr.store.elements, function( elemId ) {
+      sr.tools.forOwn( sr.store.elements, function( elemId ){
         elem    = sr.store.elements[ elemId ];
         visible = sr.isElemVisible( elem );
-        if ( visible && !elem.revealed ) {
+        if ( visible && !elem.revealed ){
 
           if ( elem.config.useDelay === 'always'
           || ( elem.config.useDelay === 'onload' && !sr.initialized )
-          || ( elem.config.useDelay === 'once'   && !elem.seen ) ) {
+          || ( elem.config.useDelay === 'once'   && !elem.seen ) ){
             elem.domEl.setAttribute( 'style',
                 elem.styles.inline
               + elem.styles.transform.target
@@ -258,7 +258,7 @@ ______________________________________________________________________________*/
           elem.seen = true;
           queueCallback( 'reveal', elem );
 
-        } else if ( !visible && elem.config.reset && elem.revealed ) {
+        } else if ( !visible && elem.config.reset && elem.revealed ){
           elem.domEl.setAttribute( 'style',
               elem.styles.inline
             + elem.styles.transform.initial
@@ -270,12 +270,12 @@ ______________________________________________________________________________*/
 
       sr.running = false;
 
-      function queueCallback( type, elem ) {
+      function queueCallback( type, elem ){
         var elapsed  = 0;
         var duration = 0;
         var callback = 'after';
 
-        switch ( type ) {
+        switch ( type ){
           case 'reveal':
             duration = elem.config.duration + elem.config.delay;
             callback += 'Reveal';
@@ -286,14 +286,14 @@ ______________________________________________________________________________*/
             break;
         }
 
-        if ( elem.timer ) {
+        if ( elem.timer ){
           elapsed = Math.abs( elem.timer.started - new Date() );
           window.clearTimeout( elem.timer.clock );
         }
 
         elem.timer = { started: new Date() };
 
-        elem.timer.clock = window.setTimeout(function() {
+        elem.timer.clock = window.setTimeout(function(){
           elem.config[ callback ]( elem.domEl );
           elem.timer = null;
         }, duration - elapsed );
@@ -301,44 +301,44 @@ ______________________________________________________________________________*/
       }
     };
 
-    ScrollReveal.prototype.getContainer = function( container ) {
-      if ( !container ) {
+    ScrollReveal.prototype.getContainer = function( container ){
+      if ( !container ){
         container = window.document.documentElement;
       }
       var w = container.clientWidth;
       var h = container.clientHeight;
       return {
-        width:  w,
-        height: h
+        width  : w,
+        height : h
       };
     };
 
-    ScrollReveal.prototype.getScrolled = function( container ) {
-      if ( !container ) {
+    ScrollReveal.prototype.getScrolled = function( container ){
+      if ( !container ){
         return {
-          x: window.pageXOffset,
-          y: window.pageYOffset
+          x : window.pageXOffset,
+          y : window.pageYOffset
         };
       } else {
         var offset = sr.getOffset( container );
         return {
-          x: container.scrollLeft + offset.left,
-          y: container.scrollTop  + offset.top
+          x : container.scrollLeft + offset.left,
+          y : container.scrollTop  + offset.top
         };
       }
     };
 
-    ScrollReveal.prototype.getOffset = function( domEl ) {
+    ScrollReveal.prototype.getOffset = function( domEl ){
       var offsetTop    = 0;
       var offsetLeft   = 0;
       var offsetHeight = domEl.offsetHeight;
       var offsetWidth  = domEl.offsetWidth;
 
       do {
-        if ( !isNaN( domEl.offsetTop ) ) {
+        if ( !isNaN( domEl.offsetTop ) ){
           offsetTop += domEl.offsetTop;
         }
-        if ( !isNaN( domEl.offsetLeft ) ) {
+        if ( !isNaN( domEl.offsetLeft ) ){
           offsetLeft += domEl.offsetLeft;
         }
       } while ( domEl = domEl.offsetParent );
@@ -351,7 +351,7 @@ ______________________________________________________________________________*/
       };
     };
 
-    ScrollReveal.prototype.isElemVisible = function( elem ) {
+    ScrollReveal.prototype.isElemVisible = function( elem ){
       var offset     = sr.getOffset( elem.domEl );
       var container  = sr.getContainer( elem.config.container );
       var scrolled   = sr.getScrolled( elem.config.container );
@@ -366,7 +366,7 @@ ______________________________________________________________________________*/
 
       return ( confirmBounds() || isPositionFixed() );
 
-      function confirmBounds() {
+      function confirmBounds(){
         var top        = elemTop    + elemHeight * vF;
         var left       = elemLeft   + elemWidth  * vF;
         var bottom     = elemBottom - elemHeight * vF;
@@ -383,14 +383,14 @@ ______________________________________________________________________________*/
             && ( right  < viewRight  );
       }
 
-      function isPositionFixed() {
+      function isPositionFixed(){
         return ( window.getComputedStyle( elem.domEl ).position === 'fixed' );
       }
     };
 
-    ScrollReveal.prototype.sync = function() {
-      if ( sr.history.length ) {
-        for ( var i = 0; i < sr.history.length; i++ ) {
+    ScrollReveal.prototype.sync = function(){
+      if ( sr.history.length ){
+        for ( var i = 0; i < sr.history.length; i++ ){
           var record = sr.history[ i ];
           sr.reveal( record.selector, record.config, true );
         };
@@ -405,28 +405,28 @@ ______________________________________________________________________________*/
 
   })();
 
-  var Tools = (function() {
+  var Tools = (function(){
 
-    Tools.prototype.isObject = function( object ) {
+    Tools.prototype.isObject = function( object ){
       return object !== null && typeof object === 'object' && object.constructor == Object;
     };
 
-    Tools.prototype.forOwn = function( object, callback ) {
-      if ( !this.isObject( object ) ) {
+    Tools.prototype.forOwn = function( object, callback ){
+      if ( !this.isObject( object ) ){
         throw new TypeError('Expected \'object\', but received \'' + typeof object + '\'.');
       } else {
-        for ( var property in object ) {
-          if ( object.hasOwnProperty( property ) ) {
+        for ( var property in object ){
+          if ( object.hasOwnProperty( property ) ){
             callback( property );
           }
         }
       }
     };
 
-    Tools.prototype.extend = function( target, source ) {
-      this.forOwn( source, function( property ) {
-        if ( this.isObject( source[ property ] ) ) {
-          if ( !target[ property ] || !this.isObject( target[ property ] ) ) {
+    Tools.prototype.extend = function( target, source ){
+      this.forOwn( source, function( property ){
+        if ( this.isObject( source[ property ] ) ){
+          if ( !target[ property ] || !this.isObject( target[ property ] ) ){
             target[ property ] = {};
           }
           this.extend( target[ property ], source[ property ] );
@@ -437,28 +437,28 @@ ______________________________________________________________________________*/
       return target;
     };
 
-    Tools.prototype.extendClone = function( target, source ) {
+    Tools.prototype.extendClone = function( target, source ){
       return this.extend( this.extend( {}, target ), source );
     };
 
-    Tools.prototype.isMobile = function() {
+    Tools.prototype.isMobile = function(){
       return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test( navigator.userAgent );
     };
 
-    Tools.prototype.isSupported = function( feature ) {
+    Tools.prototype.isSupported = function( feature ){
       var sensor    = document.createElement('sensor');
       var cssPrefix = 'Webkit,Moz,O,'.split(',');
       var tests     = ( feature + cssPrefix.join( feature + ',' ) ).split(',');
 
-      for ( var i = 0; i < tests.length; i++ ) {
-        if ( !sensor.style[ tests[ i ] ] === '' ) {
+      for ( var i = 0; i < tests.length; i++ ){
+        if ( !sensor.style[ tests[ i ] ] === '' ){
           return false;
         }
       }
       return true;
     };
 
-    function Tools() {};
+    function Tools(){};
     return Tools;
 
   })();
