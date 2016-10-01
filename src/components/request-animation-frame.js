7fa017414395762d0475b7cb3479aa8cc0a27819
J.L. Makes <julianlloyd@gmail.com>
2016-10-01 19:29:28 +0200

    extract requestAnimationFrame polyfill into a module

---
+++ src/components/request-animation-frame.js
@@ -0,0 +1,28 @@
+(function () {
+  const polyfill = (() => {
+    let timeLast = 0
+
+    return (callback) => {
+      let timeCurrent = Date.now()
+      let timeDelta
+
+      /**
+       * Dynamically set the delay on a per-tick basis to more closely match 60fps.
+       * Technique by Erik Moller. MIT license: https://gist.github.com/paulirish/1579671
+       */
+      timeDelta = Math.max(0, 16 - (timeCurrent - timeLast))
+      timeLast = timeCurrent + timeDelta
+
+      return window.setTimeout(() => {
+        callback(timeCurrent + timeDelta)
+      }, timeDelta)
+    }
+  })()
+
+  const requestAnimationFrame = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    polyfill
+})();
+
+export default requestAnimationFrame
