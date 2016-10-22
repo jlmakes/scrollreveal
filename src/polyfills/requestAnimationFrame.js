2a205d333347af9f49bcca82d526ed3d0474305a
J.L. Makes <julianlloyd@gmail.com>
2016-10-22 14:30:11 +0200

    move and rename requestAnimationFrame polyfill

---
+++ src/polyfills/requestAnimationFrame.js
@@ -0,0 +1,27 @@
+
+
+const polyfill = (() => {
+  let timeLast = 0;
+
+  return (callback) => {
+    /**
+     * Dynamically set the delay on a per-tick basis to more closely match 60fps.
+     * Technique by Erik Moller. MIT license: https://gist.github.com/paulirish/1579671
+     */
+    const timeCurrent = Date.now();
+    const timeDelta = Math.max(0, 16 - (timeCurrent - timeLast));
+    timeLast = timeCurrent + timeDelta;
+
+    return window.setTimeout(() => {
+      callback(timeCurrent + timeDelta);
+    }, timeDelta);
+  };
+})();
+
+const requestAnimationFrame = window.requestAnimationFrame ||
+                              window.webkitRequestAnimationFrame ||
+                              window.mozRequestAnimationFrame ||
+                              polyfill;
+
+
+export default requestAnimationFrame;
