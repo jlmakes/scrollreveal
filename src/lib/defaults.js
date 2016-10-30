dd41e6836d84f0838a3b335cbeb8d2647dafbba2
J.L. Makes <julianlloyd@gmail.com>
2016-10-30 23:31:30 +0100

    create new library defaults module

---
+++ src/lib/defaults.js
@@ -0,0 +1,31 @@
+
+
+export default {
+  origin: 'bottom',
+  distance: '0',
+  duration: 300,
+  delay: 0,
+  rotate: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
+  opacity: 0,
+  scale: 1,
+  easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+  container: document.documentElement,
+  mobile: true,
+  reset: false,
+  useDelay: 'always',
+  viewFactor: 0.2,
+  viewOffset: {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+  beforeReveal () {},
+  beforeReset () {},
+  afterReveal () {},
+  afterReset () {},
+};
