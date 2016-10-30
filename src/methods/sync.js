80d6f73c9f571a2fb7f2644866391e5c60d9db38
J.L. Makes <julianlloyd@gmail.com>
2016-10-30 23:33:44 +0100

    create new sync method module

---
+++ src/methods/sync.js
@@ -0,0 +1,21 @@
+
+
+import { initialize } from '../lib/functions';
+import reveal from './reveal';
+
+/**
+ * Re-runs `reveal()` for each record stored in history, effectively capturing
+ * any content loaded asynchronously that matches existing reveal set targets.
+ * @return {object} - The current ScrollReveal instance.
+ */
+function sync () {
+  this.store.history.forEach((record) => {
+    reveal.call(this, record.target, record.config, record.interval, false);
+  });
+
+  initialize.call(this);
+
+  return this;
+}
+
+export default sync;
