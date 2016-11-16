3a6dedb6cc1684775ec05a758956de43d619f91c
J.L. Makes <julianlloyd@gmail.com>
2016-11-16 06:03:58 +0100

    add constructor test cases

---
+++ test/main.spec.js
@@ -0,0 +1,55 @@
+/* global describe, it, expect */
+import ScrollReveal from '../src/main';
+
+describe('ScrollReveal', () => {
+
+  describe('Constructor', () => {
+
+    it('should return a new instance with `new` keyword', () => {
+      const result = new ScrollReveal();
+      expect(result).to.exist;
+    });
+
+    it('should return a new instance without `new` keyword', () => {
+      const result = ScrollReveal();
+      expect(result).to.exist;
+    });
+
+    it('should add the class `sr` to `<html>` element', () => {
+      const sr = new ScrollReveal();
+      const result = document.documentElement.classList.contains('sr');
+      expect(result).to.be.true;
+    });
+  });
+
+  describe('Instance', () => {
+
+    it('should have a `remove` method', () => {
+      const sr = new ScrollReveal();
+      const result = sr.remove;
+      expect(result).to.exist;
+      expect(result).to.be.a('function');
+    });
+
+    it('should have a `reveal` method', () => {
+      const sr = new ScrollReveal();
+      const result = sr.reveal;
+      expect(result).to.exist;
+      expect(result).to.be.a('function');
+    });
+
+    it('should have a `sync` method', () => {
+      const sr = new ScrollReveal();
+      const result = sr.sync;
+      expect(result).to.exist;
+      expect(result).to.be.a('function');
+    });
+
+    it('should have a `watch` method', () => {
+      const sr = new ScrollReveal();
+      const result = sr.watch;
+      expect(result).to.exist;
+      expect(result).to.be.a('function');
+    });
+  });
+});
