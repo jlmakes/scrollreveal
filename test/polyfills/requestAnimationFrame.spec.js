04ea890098808c05d870a74779c9d8d697404ad3
J.L. Makes <julianlloyd@gmail.com>
2016-11-16 08:04:32 +0100

    add requestAnimationFrame polyfill test cases

--- test/polyfills/requestAnimationFrame.spec.js
+++ test/polyfills/requestAnimationFrame.spec.js
@@ -1,17 +1,30 @@
-/* global describe, it, expect */
-import requestAnimationFrame from '../../src/polyfills/requestAnimationFrame';
+/* global describe, it, expect, sinon */
+import { polyfill } from '../../src/polyfills/requestAnimationFrame';
 
 describe('Polyfills', () => {
 
   describe('requestAnimationFrame()', () => {
 
     it('should be function', () => {
-      const result = requestAnimationFrame;
-      expect(result).to.be.a('function');
+      expect(polyfill).to.be.a('function');
     });
 
-    // it('should throttle callback calls', () => {
-    //   // ...
-    // });
+    it('should invoke callback when called', () => {
+      const spy = sinon.spy();
+      polyfill(spy);
+      setTimeout(() => {
+        expect(spy).to.have.been.called;
+      }, 0);
+    });
+
+    it('should throttle callback invocations', () => {
+      const spy = sinon.spy();
+      polyfill(spy);
+      polyfill(spy);
+      polyfill(spy);
+      setTimeout(() => {
+        expect(spy).to.have.been.calledOnce;
+      }, 75);
+    });
   });
 });
