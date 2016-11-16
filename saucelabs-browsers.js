d042384f40e835d741c996f057100249e2b7d014
J.L. Makes <julianlloyd@gmail.com>
2016-11-16 06:39:37 +0100

    update which browsers are tested by sauce labs

--- saucelabs-browsers.js
+++ saucelabs-browsers.js
@@ -2,42 +2,14 @@ module.exports = {
 
   // Chrome
 
-  sl_linux_chrome_48: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'linux',
-    version: '48',
-  },
-
   sl_windows_chrome_54: {
     base: 'SauceLabs',
     browserName: 'chrome',
-    platform: 'Windows 10',
-    version: '48',
-  },
-
-  sl_osx_chrome_54: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'OS X 10.10',
-    version: '48',
+    platform: 'Windows 7',
+    version: '54',
   },
 
   // Safari
-
-  sl_osx_safari_10: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.11',
-    version: '10',
-  },
-
-  sl_osx_safari_9: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.11',
-    version: '9',
-  },
 
   sl_osx_safari_8: {
     base: 'SauceLabs',
@@ -47,20 +19,6 @@ module.exports = {
   },
 
   // Firefox
-
-  sl_osx_firefox_49: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'OS X 10.9',
-    version: '45',
-  },
-
-  sl_windows_firefox_49: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 8.1',
-    version: '45',
-  },
 
   sl_linux_firefox_45: {
     base: 'SauceLabs',
@@ -96,13 +54,6 @@ module.exports = {
 
   // MS Edge
 
-  sl_windows_edge_13: {
-    base: 'SauceLabs',
-    browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version: '13.10586',
-  },
-
   sl_windows_edge_14: {
     base: 'SauceLabs',
     browserName: 'MicrosoftEdge',
@@ -112,7 +63,7 @@ module.exports = {
 
   // // Android
 
-  sl_android_5_1: {
+  sl_android_5: {
     base: 'SauceLabs',
     browserName: 'android',
     platform: 'Linux',
