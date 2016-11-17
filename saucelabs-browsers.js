183887e3c0edd5aa05363b11d78a8629fb3faf96
J.L. Makes <julianlloyd@gmail.com>
2016-11-17 09:56:26 +0100

    update which browsers are tested by sauce labs
    Encore! I'm still experimenting with test times, and to be frank,
    what looks good in the README. I've decided to test one relatively
    old version of each platform, in good faith of support in future
    versions.
    
    Also, Sauce Labs takes over 4 minutes to spin up Mobile Safari 10,
    which just seemed pointless. I think this is the last time I'll
    have to fiddle with the SauceLabs browser config.
    
    'Ey!

--- saucelabs-browsers.js
+++ saucelabs-browsers.js
@@ -1,21 +1,12 @@
 module.exports = {
 
-  // Chrome
+  // Android
 
-  sl_windows_chrome_54: {
+  sl_android_4: {
     base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 7',
-    version: '54',
-  },
-
-  // Safari
-
-  sl_osx_safari_8: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.10',
-    version: '8',
+    browserName: 'android',
+    platform: 'Linux',
+    version: '4.2',
   },
 
   // Firefox
@@ -27,6 +18,15 @@ module.exports = {
     version: '45',
   },
 
+  // Chrome
+
+  sl_windows_chrome_54: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 7',
+    version: '50',
+  },
+
   // Internet explorer
 
   sl_ie_10: {
@@ -36,11 +36,13 @@ module.exports = {
     version: '10',
   },
 
-  sl_ie_11: {
+  // iOS iPhone
+
+  sl_ios_8: {
     base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11',
+    browserName: 'iphone',
+    platform: 'OS X 10.10',
+    version: '8.1',
   },
 
   // MS Edge
@@ -52,42 +54,21 @@ module.exports = {
     version: '13.10586',
   },
 
-  // // Android
-
-  sl_android_4: {
+  // Opera
+  //
+  sl_windows_opera_12: {
     base: 'SauceLabs',
-    browserName: 'android',
-    platform: 'Linux',
-    version: '4.2',
+    browserName: 'opera',
+    platform: 'Windows 7',
+    version: '12.12',
   },
 
-  sl_android_5: {
-    base: 'SauceLabs',
-    browserName: 'android',
-    platform: 'Linux',
-    version: '5.1',
-  },
+  // Safari
 
-  // // iOS iPhone
-
-  sl_ios_8: {
+  sl_osx_safari_8: {
     base: 'SauceLabs',
-    browserName: 'iphone',
+    browserName: 'safari',
     platform: 'OS X 10.10',
-    version: '8.4',
-  },
-
-  l_ios_9: {
-    base: 'SauceLabs',
-    browserName: 'iphone',
-    platform: 'OS X 10.10',
-    version: '9.2',
-  },
-
-  sl_ios_10: {
-    base: 'SauceLabs',
-    browserName: 'iphone',
-    platform: 'OS X 10.11',
-    version: '10.0',
+    version: '8',
   },
 };
