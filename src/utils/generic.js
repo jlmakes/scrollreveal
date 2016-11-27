c3187e761ad086ecbd400ca234ba3d1b73c804a9
J.L. Makes <julianlloyd@gmail.com>
2016-11-27 21:07:53 +0100

    remove jsdoc block from nextUniqueId()

--- src/utils/generic.js
+++ src/utils/generic.js
@@ -4,10 +4,6 @@ export function logger (message) {
 	}
 }
 
-/**
-* Sequential number generator for unique IDs.
-* @return {number}
-*/
 export const nextUniqueId = (() => {
 	let uid = 0;
 	return () => uid++;
