5b7fcb0e5fd710aca86ca5a62abd151a9fee1d94
J.L. Makes <julianlloyd@gmail.com>
2016-12-13 23:06:45 +0100

    alphabetize matrix utilities

--- src/utils/matrix.js
+++ src/utils/matrix.js
@@ -9,6 +9,20 @@ export default {
 	},
 
 
+	multiply (m, x) {
+		const result = [];
+		for (let i = 0; i < 4; i++) {
+			const r = [m[i], m[i + 4], m[i + 8], m[i + 12]];
+			for (let j = 0; j < 4; j++) {
+				const k = j * 4;
+				const c = [x[k], x[k + 1], x[k + 2], x[k + 3]];
+				result[i + k] = r[0] * c[0] + r[1] * c[1] + r[2] * c[2] + r[3] * c[3];
+			}
+		}
+		return result;
+	},
+
+
 	rotateX (theta) {
 		const angle = Math.PI / 180 * theta;
 		const matrix = this.identity();
@@ -57,19 +71,5 @@ export default {
 		const matrix = this.identity();
 		matrix[10] = scalar;
 		return matrix;
-	},
-
-
-	multiply (m, x) {
-		const result = [];
-		for (let i = 0; i < 4; i++) {
-			const r = [m[i], m[i + 4], m[i + 8], m[i + 12]];
-			for (let j = 0; j < 4; j++) {
-				const k = j * 4;
-				const c = [x[k], x[k + 1], x[k + 2], x[k + 3]];
-				result[i + k] = r[0] * c[0] + r[1] * c[1] + r[2] * c[2] + r[3] * c[3];
-			}
-		}
-		return result;
 	},
 };
