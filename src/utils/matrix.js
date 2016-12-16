531f44f03ab3b6a73cdf079a11e34652e7558043
J.L. Makes <julianlloyd@gmail.com>
2016-12-16 03:05:32 +0100

    update precision of matrix utilities

--- src/utils/matrix.js
+++ src/utils/matrix.js
@@ -10,25 +10,26 @@ export default {
 
 
 	multiply (m, x) {
-		const result = [];
+		const sum = [];
 		for (let i = 0; i < 4; i++) {
 			const r = [m[i], m[i + 4], m[i + 8], m[i + 12]];
 			for (let j = 0; j < 4; j++) {
 				const k = j * 4;
 				const c = [x[k], x[k + 1], x[k + 2], x[k + 3]];
-				result[i + k] = r[0] * c[0] + r[1] * c[1] + r[2] * c[2] + r[3] * c[3];
+				let result = r[0] * c[0] + r[1] * c[1] + r[2] * c[2] + r[3] * c[3];
+				sum[i + k] = parseFloat(result.toPrecision(6));
 			}
 		}
-		return result;
+		return sum;
 	},
 
 
 	rotateX (theta) {
 		const angle = Math.PI / 180 * theta;
 		const matrix = this.identity();
-		matrix[5] = matrix[10] = parseFloat(Math.cos(angle).toFixed(6));
-		matrix[6] = parseFloat(Math.sin(angle).toFixed(6));
-		matrix[9] = parseFloat(Math.sin(angle).toFixed(6)) * -1;
+		matrix[5] = matrix[10] = parseFloat(Math.cos(angle).toPrecision(6));
+		matrix[6] = parseFloat(Math.sin(angle).toPrecision(6));
+		matrix[9] = parseFloat(Math.sin(angle).toPrecision(6)) * -1;
 		return matrix;
 	},
 
@@ -36,9 +37,9 @@ export default {
 	rotateY (theta) {
 		const angle = Math.PI / 180 * theta;
 		const matrix = this.identity();
-		matrix[0] = matrix[10] = parseFloat(Math.cos(angle).toFixed(6));
-		matrix[8] = parseFloat(Math.sin(angle).toFixed(6));
-		matrix[2] = parseFloat(Math.sin(angle).toFixed(6)) * -1;
+		matrix[0] = matrix[10] = parseFloat(Math.cos(angle).toPrecision(6));
+		matrix[8] = parseFloat(Math.sin(angle).toPrecision(6));
+		matrix[2] = parseFloat(Math.sin(angle).toPrecision(6)) * -1;
 		return matrix;
 	},
 
@@ -46,9 +47,9 @@ export default {
 	rotateZ (theta) {
 		const angle = Math.PI / 180 * theta;
 		const matrix = this.identity();
-		matrix[0] = matrix[5] = parseFloat(Math.cos(angle).toFixed(6));
-		matrix[1] = parseFloat(Math.sin(angle).toFixed(6));
-		matrix[4] = parseFloat(Math.sin(angle).toFixed(6)) * -1;
+		matrix[0] = matrix[5] = parseFloat(Math.cos(angle).toPrecision(6));
+		matrix[1] = parseFloat(Math.sin(angle).toPrecision(6));
+		matrix[4] = parseFloat(Math.sin(angle).toPrecision(6)) * -1;
 		return matrix;
 	},
 
