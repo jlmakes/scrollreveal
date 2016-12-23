81232fb9bc967415869f33fd8f8a022935ba7ab7
J.L. Makes <julianlloyd@gmail.com>
2016-12-24 00:17:16 +0100

    throw errors from generic utilities

--- src/utils/generic.js
+++ src/utils/generic.js
@@ -1,36 +1,44 @@
 export function deepAssign (target, ...sources) {
-	sources.forEach((source) => {
-		forOwn(source, (property) => {
-			if (isObject(source[property])) {
-				if (!target[property] || !isObject(target[property])) {
-					target[property] = {};
+	if (isObject(target)) {
+		sources.forEach((source) => {
+			forOwn(source, (property) => {
+				if (isObject(source[property])) {
+					if (!target[property] || !isObject(target[property])) {
+						target[property] = {};
+					}
+					deepAssign(target[property], source[property]);
+				} else {
+					target[property] = source[property];
 				}
-				deepAssign(target[property], source[property]);
-			} else {
-				target[property] = source[property];
-			}
+			});
 		});
-	});
-	return target;
+		return target;
+	} else {
+		throw new TypeError('Expected an object literal.');
+	}
 }
 
 
 export function deepEqual (first, second) {
-	let bool = true;
-	forOwn(second, (property) => {
-		if (bool) {
-			if (isObject(second[property])) {
-				bool = deepEqual(first[property], second[property]);
-			} else if (first[property] !== second[property]) bool = false;
-		}
-	});
-	return bool;
+	if (isObject(first)) {
+		let bool = true;
+		forOwn(second, (property) => {
+			if (bool) {
+				if (isObject(second[property])) {
+					bool = deepEqual(first[property], second[property]);
+				} else if (first[property] !== second[property]) bool = false;
+			}
+		});
+		return bool;
+	} else {
+		throw new TypeError('Expected an object literal.');
+	}
 }
 
 
 export function isObject (object) {
-	return object !== null && typeof object === 'object' &&
-		(object.constructor === Object || Object.prototype.toString.call(object) === '[object Object]');
+	return object !== null && typeof object === 'object'
+		&& (object.constructor === Object || Object.prototype.toString.call(object) === '[object Object]');
 }
 
 
