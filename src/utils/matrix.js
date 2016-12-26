49b8d728d9f995f5d82dda36a2eb9ae0816cbd13
J.L. Makes <julianlloyd@gmail.com>
2016-12-26 18:39:46 +0100

    improve transformation matrices for accuracy

--- src/utils/matrix.js
+++ src/utils/matrix.js
@@ -56,9 +56,11 @@ export function multiply (m, x) {
 export function rotateX (theta) {
 	const angle = Math.PI / 180 * theta
 	const matrix = identity()
-	matrix[5] = matrix[10] = parseFloat(Math.cos(angle).toPrecision(6))
-	matrix[6] = parseFloat(Math.sin(angle).toPrecision(6))
-	matrix[9] = parseFloat(Math.sin(angle).toPrecision(6)) * -1
+
+	matrix[5] = matrix[10] = Math.cos(angle)
+	matrix[6] = matrix[9] = Math.sin(angle)
+	matrix[9] *= -1
+
 	return matrix
 }
 
@@ -66,9 +68,11 @@ export function rotateX (theta) {
 export function rotateY (theta) {
 	const angle = Math.PI / 180 * theta
 	const matrix = identity()
-	matrix[0] = matrix[10] = parseFloat(Math.cos(angle).toPrecision(6))
-	matrix[8] = parseFloat(Math.sin(angle).toPrecision(6))
-	matrix[2] = parseFloat(Math.sin(angle).toPrecision(6)) * -1
+
+	matrix[0] = matrix[10] = Math.cos(angle)
+	matrix[2] = matrix[8] = Math.sin(angle)
+	matrix[2] *= -1
+
 	return matrix
 }
 
@@ -76,9 +80,11 @@ export function rotateY (theta) {
 export function rotateZ (theta) {
 	const angle = Math.PI / 180 * theta
 	const matrix = identity()
-	matrix[0] = matrix[5] = parseFloat(Math.cos(angle).toPrecision(6))
-	matrix[1] = parseFloat(Math.sin(angle).toPrecision(6))
-	matrix[4] = parseFloat(Math.sin(angle).toPrecision(6)) * -1
+
+	matrix[0] = matrix[5] = Math.cos(angle)
+	matrix[1] = matrix[4] = Math.sin(angle)
+	matrix[4] *= -1
+
 	return matrix
 }
 
