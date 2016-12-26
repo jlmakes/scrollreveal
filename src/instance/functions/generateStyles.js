7edceca49d8e1d7c902a3beef8b2676a4e03431d
J.L. Makes <julianlloyd@gmail.com>
2016-12-26 02:28:00 +0100

    fix inline style generator regression

--- src/instance/functions/generateStyles.js
+++ src/instance/functions/generateStyles.js
@@ -5,15 +5,15 @@ export default function generateStyles (element) {
 	const computed = window.getComputedStyle(element.node)
 	const config = element.config
 
-	const inlineRegex = /(.+[^])?$/g
+	const inlineRegex = /(.+);*$/g
 	const inlineStyle = element.node.getAttribute('style')
 	const inlineMatch = inlineRegex.exec(inlineStyle)[1]
 
 	const inline = {
 		computed: inlineMatch,
 		generated: (inlineMatch)
-			? `${inlineMatch} visibility: visible`
-			: 'visibility: visible',
+			? `${inlineMatch}; visibility: visible;`
+			: 'visibility: visible;',
 	}
 
 	let opacity
