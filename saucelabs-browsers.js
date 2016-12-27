c56b969965707f8b1ff7b54f09449d635bed2f8f
J.L. Makes <julianlloyd@gmail.com>
2016-12-27 05:07:58 +0100

    update which browsers are tested by sauce labs

--- saucelabs-browsers.js
+++ saucelabs-browsers.js
@@ -1,66 +1,30 @@
 module.exports = {
 
-	// Android
+	// Evergreen
 
-	sl_android_4: {
-		base: 'SauceLabs',
-		browserName: 'android',
-		platform: 'Linux',
-		version: '4.2',
-	},
-
-	// Firefox
-
-	sl_linux_firefox_45: {
-		base: 'SauceLabs',
-		browserName: 'firefox',
-		platform: 'linux',
-		version: '45',
-	},
-
-	sl_osx_firefox_45: {
-		base: 'SauceLabs',
-		browserName: 'firefox',
-		platform: 'OS X 10.10',
-		version: '45',
-	},
-
-	sl_windows_firefox_45: {
-		base: 'SauceLabs',
-		browserName: 'firefox',
-		platform: 'Windows 7',
-		version: '45',
-	},
-
-	// Chrome
-
-	sl_linux_chrome_48: {
-		base: 'SauceLabs',
-		browserName: 'chrome',
-		platform: 'linux',
-		version: '48',
-	},
-
-	sl_osx_chrome_48: {
-		base: 'SauceLabs',
-		browserName: 'chrome',
-		platform: 'OS X 10.10',
-		version: '48',
-	},
-
-	sl_windows_chrome_48: {
+	sl_chrome: {
 		base: 'SauceLabs',
 		browserName: 'chrome',
 		platform: 'Windows 7',
-		version: '48',
 	},
 
-	// Internet explorer
+	sl_firefox: {
+		base: 'SauceLabs',
+		browserName: 'firefox',
+	},
+
+	sl_mac_safari: {
+		base: 'SauceLabs',
+		browserName: 'safari',
+		platform: 'OS X 10.10',
+	},
+
+	// Internet Explorer
 
 	sl_ie_10: {
 		base: 'SauceLabs',
 		browserName: 'internet explorer',
-		platform: 'Windows 7',
+		platform: 'Windows 8',
 		version: '10',
 	},
 
@@ -71,51 +35,35 @@ module.exports = {
 		version: '11',
 	},
 
-	// iOS iPhone
+	sl_edge: {
+		base: 'SauceLabs',
+		browserName: 'MicrosoftEdge',
+		platform: 'Windows 10',
+	},
+
+	// Mobile
 
 	sl_ios_8: {
 		base: 'SauceLabs',
 		browserName: 'iphone',
-		platform: 'OS X 10.10',
 		version: '8.4',
 	},
 
 	sl_ios_9: {
 		base: 'SauceLabs',
 		browserName: 'iphone',
-		platform: 'OS X 10.10',
 		version: '9.3',
 	},
 
-	// MS Edge
-
-	sl_edge_13: {
+	sl_android_4: {
 		base: 'SauceLabs',
-		browserName: 'MicrosoftEdge',
-		platform: 'Windows 10',
-		version: '13.10586',
+		browserName: 'android',
+		version: '4.2',
 	},
 
-	// Safari
-
-	sl_osx_safari_8: {
+	sl_android_5: {
 		base: 'SauceLabs',
-		browserName: 'safari',
-		platform: 'OS X 10.10',
-		version: '8',
+		browserName: 'android',
+		version: '5.1',
 	},
-
-	sl_osx_safari_9: {
-		base: 'SauceLabs',
-		browserName: 'safari',
-		platform: 'OS X 10.11',
-		version: '9',
-	},
-
-	sl_osx_safari_10: {
-		base: 'SauceLabs',
-		browserName: 'safari',
-		platform: 'OS X 10.11',
-		version: '10',
-	},
-};
+}
