43f69bd33bcc98f8472fc9d48f8bad390cce9631
J.L. Makes <julianlloyd@gmail.com>
2017-01-21 17:00:32 +0100

    update element geometry and scroll positions in event handler

--- src/instance/functions/delegate.js
+++ src/instance/functions/delegate.js
@@ -1,7 +1,28 @@
 import { requestAnimationFrame } from '../../polyfills/requestAnimationFrame'
+import { getGeometry, getScrolled } from '../../utils/core'
+import { each } from '../../utils/generic'
 import animate from './animate'
 
+export default function delegate (event = {}) {
+	requestAnimationFrame(() => {
+		const containers = this.store.containers
+		const elements = this.store.elements
 
-export default function delegate () {
-	requestAnimationFrame(animate.bind(this))
+		switch (event.type) {
+
+			case 'scroll':
+				each(containers, container => container.scroll = getScrolled.call(this, container))
+				break
+
+			case 'resize':
+			default:
+				each(containers, container => {
+					container.geometry = getGeometry.call(this, container)
+					container.scroll = getScrolled.call(this, container)
+				})
+				each(elements, element => element.geometry = getGeometry.call(this, element))
+		}
+
+		animate.call(this)
+	})
 }
