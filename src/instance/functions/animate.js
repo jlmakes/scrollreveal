f89da6dcdfdd082c1cf93724388d855b564b0e33
J.L. Makes <julianlloyd@gmail.com>
2017-01-22 18:28:10 +0100

    add basic style handling to animate method

--- src/instance/functions/animate.js
+++ src/instance/functions/animate.js
@@ -1,3 +1,26 @@
+import { isElementVisible } from '../../utils/core'
+
+
 export default function animate (element) {
-	// things...
+
+	let styles
+
+	if (isElementVisible.call(this, element)) {
+		styles = [
+			element.styles.inline.generated,
+			element.styles.opacity.computed,
+			element.styles.transform.generated.final,
+			element.styles.transition.generated.instant,
+		].join(' ')
+
+	} else {
+		styles = [
+			element.styles.inline.generated,
+			element.styles.opacity.generated,
+			element.styles.transform.generated.initial,
+			element.styles.transition.generated.instant,
+		].join(' ')
+	}
+
+	element.node.setAttribute('style', styles)
 }
