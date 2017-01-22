39d564c8892836612d2fb87755546b1aeaa651e9
J.L. Makes <julianlloyd@gmail.com>
2017-01-22 17:56:32 +0100

    add sample comment banner to source index
    After bundling the banner ends up at the bottom though, and using
    `options.banner` inside the rollup config didn't seem to work with
    a template string.
    
    This needs a second draft, and to be moved to the top of the
    generated bundles.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+/////    /////    /////    /////
+/////    /////    /////    /////
+/////    /////    /////    /////
+/////    /////    /////    /////
+/////             /////    /////
+/////             /////    /////
+/////    /////    /////    /////
+/////    /////    /////    /////
+         /////    /////
+         /////    /////
+/////    /////    /////    /////
+/////    /////    /////    /////
+/////    /////    /////    /////
+/////    /////    /////    /////
+
+/*!
+ * ScrollReveal
+ * ------------
+ * Website : https://scrollreveal.com
+ * Support : https://github.com/jlmakes/scrollreveal/issues
+ * Author  : https://twitter.com/jlmakes
+ *
+ * Licensed under the GNU General Public License 3.0 for
+ * compatible open source projects and non-commercial use.
+ *
+ * For commercial sites, themes, projects, and applications,
+ * keep your source code proprietary and please purchase a
+ * commercial license from https://scrollreveal.com
+ *
+ * Copyright (c) 2014–2017 Julian Lloyd. All rights reserved.
+ */
 import Constructor from './instance/constructor'
 
 export default Constructor
