788a36d1c8f85764d7f22f45acb08d3899af9f13
J.L. Makes <julianlloyd@gmail.com>
2017-01-22 17:30:59 +0100

    add comments to initialization flow

--- src/instance/functions/initialize.js
+++ src/instance/functions/initialize.js
@@ -4,6 +4,10 @@ import { each } from '../../utils/generic'
 
 export default function initialize () {
 
+	/**
+	 * Let's take stock of which containers and sequences
+	 * our current store of elements is actively using.
+	 */
 	let activeContainerIds = []
 	let activeSequenceIds = []
 
@@ -16,6 +20,9 @@ export default function initialize () {
 		}
 	})
 
+	/**
+	 * Remove unused sequences.
+	 */
 	each(this.store.sequences, sequence => {
 		if (activeSequenceIds.indexOf(sequence.id) === -1) {
 			delete this.store.sequences[sequence.id]
@@ -24,15 +31,25 @@ export default function initialize () {
 
 	each(this.store.containers, container => {
 
+		/**
+		 * Remove unused containers.
+		 */
 		if (activeContainerIds.indexOf(container.id) === -1) {
 			container.node.removeEventListener('scroll', delegate)
 			container.node.removeEventListener('resize', delegate)
 			delete this.store.containers[container.id]
 
+		/**
+		 * The default container is the <html> element, and in
+		 * this case we listen for scroll events on the window.
+		 */
 		} else if (container.node === document.documentElement) {
 			window.addEventListener('scroll', delegate.bind(this))
 			window.addEventListener('resize', delegate.bind(this))
 
+		/**
+		 * Otherwise listen for scroll events directly on the container.
+		 */
 		} else {
 			container.node.addEventListener('scroll', delegate.bind(this))
 			container.node.addEventListener('resize', delegate.bind(this))
