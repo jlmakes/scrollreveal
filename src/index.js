767b7e3d40e3a619603dffd83ead790139a13efe
J.L. Makes <julianlloyd@gmail.com>
2017-05-19 00:29:45 +0200

    update urls in source comment banner

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@
 /*!
  * ScrollReveal
  * ------------
- * Website : https://scrollreveal.com
+ * Website : https://scrollrevealjs.org
  * Support : https://github.com/jlmakes/scrollreveal/issues
  * Author  : https://twitter.com/jlmakes
  *
@@ -25,7 +25,7 @@
  *
  * For commercial sites, themes, projects, and applications,
  * keep your source code proprietary and please purchase a
- * commercial license from https://scrollreveal.com
+ * commercial license from https://scrollrevealjs.org
  *
  * Copyright (c) 2014–2017 Julian Lloyd. All rights reserved.
  */
