c6b95c81e7da571d0d0d403507c1e244bfdb66df
J.L. Makes <julianlloyd@gmail.com>
2017-07-31 22:55:25 +0200

    remove sequencer logic from animate fn

--- src/instance/functions/animate.js
+++ src/instance/functions/animate.js
@@ -1,55 +1,17 @@
-import { each } from '../../utils/generic'
 import clean from '../methods/clean'
 
 
 export default function animate (element, sequencing) {
 
-	const sequence = (element.sequence) ? this.store.sequences[element.sequence.id] : false
 	const delayed = element.config.useDelay === 'always'
 		|| element.config.useDelay === 'onload' && this.pristine
 		|| element.config.useDelay === 'once' && !element.seen
 
-	if (sequencing) {
-		if (element.sequence.index === sequence.nose.pointer - 1 && sequence.nose.pointer > sequence.nose.index) {
-			sequence.nose.pointer--
-			queueSequenceNose.call(this, sequence)
-		} else if (element.sequence.index === sequence.tail.pointer + 1 && sequence.tail.pointer < sequence.tail.index) {
-			sequence.tail.pointer++
-			queueSequenceTail.call(this, sequence)
-		} else {
-			return
-		}
-		return triggerReveal.call(this, element, delayed)
-	}
-
 	if (element.visible && !element.revealed) {
-		if (sequence) {
-			updateSequenceIndexes.call(this, sequence)
-			if (sequence.nose.pointer === null && sequence.tail.pointer === null) {
-				sequence.nose.pointer = sequence.tail.pointer = element.sequence.index
-				queueSequenceNose.call(this, sequence)
-				queueSequenceTail.call(this, sequence)
-			} else if (element.sequence.index === sequence.nose.pointer - 1 && !sequence.nose.blocked) {
-				sequence.nose.pointer--
-				queueSequenceNose.call(this, sequence)
-			} else if (element.sequence.index === sequence.tail.pointer + 1 && !sequence.tail.blocked) {
-				sequence.tail.pointer++
-				queueSequenceTail.call(this, sequence)
-			} else {
-				return
-			}
-		}
 		return triggerReveal.call(this, element, delayed)
 	}
 
 	if (!element.visible && element.revealed && element.config.reset) {
-		if (sequence) {
-			updateSequenceIndexes.call(this, sequence)
-			if (sequence.nose.index !== Infinity && sequence.tail.index !== -Infinity) {
-				sequence.nose.pointer = Math.max(sequence.nose.pointer, sequence.nose.index)
-				sequence.tail.pointer = Math.min(sequence.tail.pointer, sequence.tail.index)
-			}
-		}
 		return triggerReset.call(this, element)
 	}
 }
@@ -113,46 +75,5 @@ function registerCallbacks (element, isDelayed) {
 				clean.call(this, element.node)
 			}
 		}, duration - elapsed),
-	}
-}
-
-
-function updateSequenceIndexes (sequence) {
-	let min = Infinity
-	let max = -Infinity
-	each(sequence.elementIds, id => {
-		const element = this.store.elements[id]
-		if (element && element.visible) {
-			min = Math.min(min, element.sequence.index)
-			max = Math.max(max, element.sequence.index)
-		}
-	})
-	sequence.nose.index = min
-	sequence.tail.index = max
-}
-
-
-function queueSequenceNose (sequence) {
-	const nextId = sequence.elementIds[sequence.nose.pointer - 1]
-	const nextElement = this.store.elements[nextId]
-	if (nextElement) {
-		sequence.nose.blocked = true
-		window.setTimeout(() => {
-			sequence.nose.blocked = false
-			animate.call(this, nextElement, true)
-		}, sequence.interval)
-	}
-}
-
-
-function queueSequenceTail (sequence) {
-	const nextId = sequence.elementIds[sequence.tail.pointer + 1]
-	const nextElement = this.store.elements[nextId]
-	if (nextElement) {
-		sequence.tail.blocked = true
-		window.setTimeout(() => {
-			sequence.tail.blocked = false
-			animate.call(this, nextElement, true)
-		}, sequence.interval)
 	}
 }
