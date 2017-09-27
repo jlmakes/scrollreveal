e49bed60fe993876bd4600b037188a0738798588
J.L. Makes <julianlloyd@gmail.com>
2017-09-27 18:39:24 +0200

    update sauce labs config

--- sauce.conf.js
+++ sauce.conf.js
@@ -1,71 +1,85 @@
-module.exports = {
+const launchers = {}
 
-	// Evergreen
+const mobileLaunchers = [
+	['iOS', '8.4', 'Safari', 'iPhone 6 Simulator'],
+	['iOS', '9.3', 'Safari', 'iPhone 6s Simulator'],
+	['iOS', '10.3', 'Safari', 'iPhone 6s Simulator'],
+	['Android', '4.4', 'Browser', 'Android Emulator'],
+	['Android', '5.0', 'Browser', 'Android Emulator'],
+	['Android', '5.1', 'Browser', 'Android Emulator'],
+	['Android', '6.0', 'Chrome', 'Android Emulator'],
+]
 
-	sl_chrome: {
-		base: 'SauceLabs',
-		browserName: 'chrome',
-		platform: 'Windows 7',
-	},
+mobileLaunchers.forEach(([platform, version, browser, device]) => {
+	const launcher = `sl_${platform}_${version}_${browser}`
+		.replace(/[^a-z0-9]/gi, '_')
+		.toLowerCase()
 
-	sl_firefox: {
-		base: 'SauceLabs',
-		browserName: 'firefox',
-	},
+	launchers[launcher] = {
+		name: `${browser}, ${platform} ${version}`,
+		platformName: platform,
+		platformVersion: version,
+		browserName: browser,
+		deviceName: device,
+		deviceOrientation: 'portrait',
+		appiumVersion: '1.6.4',
+	}
+})
 
-	sl_mac_safari: {
-		base: 'SauceLabs',
-		browserName: 'safari',
-		platform: 'OS X 10.10',
-	},
+const desktopLaunchers = [
+	['Windows 8.1', 'Internet Explorer', '11.0'],
+	['Windows 8', 'Internet Explorer', '10.0'],
+	['macOS 10.12', 'Safari', '10.0'],
+	['OS X 10.11', 'Safari', '9.0'],
+	['OS X 10.10', 'Safari', '8.0'],
+	['OS X 10.9', 'Safari', '7.0'],
+]
 
-	// Internet Explorer
+desktopLaunchers.forEach(([platform, browser, version]) => {
+	const launcher = `sl_${platform}_${browser}_${version}`
+		.replace(/[^a-z0-9]/gi, '_')
+		.toLowerCase()
 
-	sl_ie_10: {
-		base: 'SauceLabs',
-		browserName: 'internet explorer',
-		platform: 'Windows 8',
-		version: '10',
-	},
+	launchers[launcher] = {
+		name: `${browser} ${version}, ${platform}`,
+		browserName: browser,
+		version,
+		platform,
+	}
+})
 
-	sl_ie_11: {
-		base: 'SauceLabs',
-		browserName: 'internet explorer',
-		platform: 'Windows 8.1',
-		version: '11',
-	},
+const evergreenLaunchers = ['Chrome', 'Firefox', 'MicrosoftEdge']
 
-	sl_edge: {
-		base: 'SauceLabs',
-		browserName: 'MicrosoftEdge',
-		platform: 'Windows 10',
-	},
+evergreenLaunchers.forEach(browser => {
+	let pastVersions = 3
+	let browserName = browser
 
-	// Mobile
+	if (browser === 'MicrosoftEdge') {
+		pastVersions = 2
+		browserName = 'Edge'
+	}
 
-	sl_ios_8: {
-		base: 'SauceLabs',
-		browserName: 'iphone',
-		version: '8.4',
-	},
+	while (pastVersions >= 0) {
+		let postfix = pastVersions > 0 ? `-${pastVersions}` : ''
+		const version = 'latest' + postfix
 
-	sl_ios_9: {
-		base: 'SauceLabs',
-		browserName: 'iphone',
-		version: '9.3',
-	},
+		const launcher = `sl_win10_${browser}_latest${postfix}`
+			.replace(/-/g, '_')
+			.toLowerCase()
 
-	sl_android_4: {
-		base: 'SauceLabs',
-		browserName: 'Browser',
-		platform: 'Android',
-		version: '4.4',
-		device: 'Android Emulator',
-	},
+		launchers[launcher] = {
+			name: `${browserName} ${version}, Windows 10`,
+			browserName: browser,
+			version,
+			platform: 'Windows 10',
+		}
 
-	sl_android_5: {
-		base: 'SauceLabs',
-		browserName: 'android',
-		version: '5.1',
-	},
+		pastVersions--
+	}
+})
+
+for (const launcher in launchers) {
+	launchers[launcher].base = 'SauceLabs'
 }
+
+module.exports = launchers
