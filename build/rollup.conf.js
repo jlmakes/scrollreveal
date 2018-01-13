3f2a0acdc64972c2cecd5e89aa2b25d184630c00
J.L. Makes <julianlloyd@gmail.com>
2018-01-13 01:47:49 +0100

    refactor es build with external dependencies

--- build/rollup.conf.js
+++ build/rollup.conf.js
@@ -1,14 +1,23 @@
 import buble from 'rollup-plugin-buble'
 import json from 'rollup-plugin-json'
+import pkg from '../package.json'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import banner from './rollup.conf.banner'
 
-export default {
+const base = {
 	banner,
-	input: 'src/index.js',
-	plugins: [json(), nodeResolve({ jsnext: true, main: true }), buble()],
-	output: [
-		{ format: 'umd', file: 'dist/scrollreveal.js', name: 'ScrollReveal' },
-		{ format: 'es', file: 'dist/scrollreveal.es.js' }
-	]
+	input: './src/index.js',
+	plugins: [json(), nodeResolve(), buble()],
+	output: []
 }
+
+const es = Object.assign({}, base, {
+	external: [...Object.keys(pkg.dependencies || {})],
+	output: { format: 'es', file: './dist/scrollreveal.es.js' }
+})
+
+const umd = Object.assign({}, base, {
+	output: { format: 'umd', file: './dist/scrollreveal.js', name: 'ScrollReveal' }
+})
+
+export default [es, umd]
