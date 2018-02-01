3055193de9897936934ff4bfbfe04f45574672f7
J.L. Makes <julianlloyd@gmail.com>
2018-02-01 04:58:30 +0100

    update build config

--- build/rollup.conf.js
+++ build/rollup.conf.js
@@ -5,19 +5,22 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import banner from './rollup.conf.banner'
 
 const base = {
-	banner,
 	input: './src/index.js',
-	plugins: [json(), nodeResolve(), buble()],
-	output: []
+	plugins: [json(), nodeResolve(), buble()]
 }
 
 const es = Object.assign({}, base, {
 	external: [...Object.keys(pkg.dependencies || {})],
-	output: { format: 'es', file: './dist/scrollreveal.es.js' }
+	output: { banner, format: 'es', file: './dist/scrollreveal.es.js' }
 })
 
 const umd = Object.assign({}, base, {
-	output: { format: 'umd', file: './dist/scrollreveal.js', name: 'ScrollReveal' }
+	output: {
+		banner,
+		format: 'umd',
+		file: './dist/scrollreveal.js',
+		name: 'ScrollReveal'
+	}
 })
 
 export default [es, umd]
