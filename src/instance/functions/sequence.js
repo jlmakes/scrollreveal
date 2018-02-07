2d49e4bf374e39a293eb8f7a58261ab996283903
J.L. Makes <julianlloyd@gmail.com>
2018-02-07 12:50:57 +0100

    refactor sequencer
    The sequencer up until now has honored DOM order, which doesn't
    handle more unusual layouts (such as the ScrollReveal demo, or
    layouts created using CSS Grid).
    
    This refactor refocuses the sequencer on visible order, which
    for most layouts will not have any noticeable difference... but
    dramatically improves the sequencer's flexibility for layouts
    with elements outside of DOM order.

--- src/instance/functions/sequence.js
+++ src/instance/functions/sequence.js
@@ -10,55 +10,66 @@ export default function sequence(element, pristine = this.pristine) {
 		return animate.call(this, element, { reset: true })
 	}
 
-	const seq = this.store.sequences[element.sequence.id]
+	const store = this.store
+	const seq = store.sequences[element.sequence.id]
 	const i = element.sequence.index
 
 	if (seq) {
-		const visible = new SequenceModel(seq, 'visible', this.store)
-		const revealed = new SequenceModel(seq, 'revealed', this.store)
+		const visible = new SequenceModel(seq, 'visible', store)
+		const revealed = new SequenceModel(seq, 'revealed', store)
 
 		seq.models = { visible, revealed }
 
 		/**
-		 * If the sequence has no revealed members,
-		 * then we reveal the first visible element
-		 * within that sequence.
-		 *
-		 * The sequence then cues a recursive call
-		 * in both directions.
+		 * At any given time, the sequencer
+		 * needs to find these 3 elements:
+		 */
+		let currentElement
+		let nextHeadElement
+		let nextFootElement
+
+		/**
+		 * When no elements within a sequence are revealed,
+		 * these 3 elements are easily pulled from the
+		 * current visible sequence model.
 		 */
 		if (!revealed.body.length) {
-			const nextId = seq.members[visible.body[0]]
-			const nextElement = this.store.elements[nextId]
-
-			if (nextElement) {
-				cue.call(this, seq, visible.body[0], -1, pristine)
-				cue.call(this, seq, visible.body[0], +1, pristine)
-				return animate.call(this, nextElement, { reveal: true, pristine })
+			currentElement = getElement(seq, visible.body.shift(), store)
+			nextHeadElement = getElement(seq, visible.head.pop(), store)
+			nextFootElement = getElement(seq, visible.body.shift(), store)
+		} else {
+			/**
+			 * More typically though, something will be revealed
+			 * and we need to model the unrevealed elements.
+			 */
+			const unrevealed = {
+				head: visible.body.filter(x => revealed.head.indexOf(x) >= 0),
+				foot: visible.body.filter(x => revealed.foot.indexOf(x) >= 0)
 			}
+			/**
+			 * Now we can compare the current sequence index
+			 * against our new model to determine the current element.
+			 */
+			if (!seq.blocked.head && i === [...unrevealed.head].pop()) {
+				currentElement = getElement(seq, unrevealed.head.pop(), store)
+			} else if (!seq.blocked.foot && i === [...unrevealed.foot].shift()) {
+				currentElement = getElement(seq, unrevealed.foot.shift(), store)
+			}
+			/**
+			 * And the next head and foot elements are
+			 * easily pulled from our custom unrevealed model.
+			 */
+			nextHeadElement = getElement(seq, unrevealed.head.pop(), store)
+			nextFootElement = getElement(seq, unrevealed.foot.shift(), store)
 		}
 
 		/**
-		 * If our element isn’t resetting, we check the
-		 * element sequence index against the head, and
-		 * then the foot of the sequence.
+		 * Verify and animate!
 		 */
-		if (
-			!seq.blocked.head &&
-			i === [...revealed.head].pop() &&
-			i >= [...visible.body].shift()
-		) {
-			cue.call(this, seq, i, -1, pristine)
-			return animate.call(this, element, { reveal: true, pristine })
-		}
-
-		if (
-			!seq.blocked.foot &&
-			i === [...revealed.foot].shift() &&
-			i <= [...visible.body].pop()
-		) {
-			cue.call(this, seq, i, +1, pristine)
-			return animate.call(this, element, { reveal: true, pristine })
+		if (currentElement) {
+			if (nextHeadElement) cue.call(this, seq, nextHeadElement, 'head', pristine)
+			if (nextFootElement) cue.call(this, seq, nextFootElement, 'foot', pristine)
+			return animate.call(this, currentElement, { reveal: true, pristine })
 		}
 	}
 }
@@ -100,17 +111,15 @@ function SequenceModel(seq, prop, store) {
 	}
 }
 
-function cue(seq, i, direction, pristine) {
-	const blocked = ['head', null, 'foot'][1 + direction]
-	const nextId = seq.members[i + direction]
-	const nextElement = this.store.elements[nextId]
-
-	seq.blocked[blocked] = true
-
+function cue(seq, element, direction, pristine) {
+	seq.blocked[direction] = true
 	setTimeout(() => {
-		seq.blocked[blocked] = false
-		if (nextElement) {
-			sequence.call(this, nextElement, pristine)
-		}
+		seq.blocked[direction] = false
+		sequence.call(this, element, pristine)
 	}, seq.interval)
+}
+
+function getElement(seq, index, store) {
+	const id = seq.members[index]
+	return store.elements[id]
 }
